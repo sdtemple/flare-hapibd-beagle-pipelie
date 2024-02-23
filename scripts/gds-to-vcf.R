e12316efade99442579435b16cedc9aa6be41146
sdtemple <sdtemple@uw.edu>
2024-02-23 15:18:45 -0800

    snpgds vs seqgds

--- scripts/gds-to-vcf.R
+++ scripts/gds-to-vcf.R
@@ -1,12 +1,20 @@
 library(SeqArray)
+library(SNPRelate)
+library(gdsfmt)
 args=commandArgs(trailingOnly=TRUE)
 
 # via SeqArray
-filein=seqOpen(args[1])
-fileout=args[2]
-seqGDS2VCF(filein, fileout, info.var=character(0), fmt.var="GT")
-
-# library(gdsfmt)
-# library(SNPRelate)
-# # via SNPRelate
-# snpgdsGDS2VCF(gds.fn=filein,vcf.fn=fileout,method=args[4],ignore.chr.prefix=args[3])
+tryCatch({
+  filein=seqOpen(args[1])
+  fileout=args[2]
+  seqGDS2VCF(filein, fileout, info.var=character(0), fmt.var="GT")
+  seqClose(args[1])
+}, error=function(e) {
+  print(e)
+  filein=snpgdsOpen(args[1])
+  seqSNP2GDS(filein,paste(args[1],".seq.gds",sep=""))
+  snpgdsClose(filein)
+  filein=seqOpen(paste(args[1],".seq.gds",sep=""))
+  seqGDS2VCF(filein, fileout, info.var=character(0), fmt.var="GT")
+  seqClose(filein)
+})

d3a40c5ed124999a0430103c682cee3bcea2c485
sdtemple <sdtemple@uw.edu>
2024-02-23 13:57:22 -0800

    info.import issue in seqarray

--- scripts/vcf-to-gds.R
+++ scripts/vcf-to-gds.R
@@ -4,4 +4,5 @@ args=commandArgs(trailingOnly=TRUE)
 # via SeqArray
 filein=args[1]
 fileout=args[2]
-seqVCF2GDS(filein, fileout)
+info.import=c("AC","AF","DP")
+seqVCF2GDS(filein, fileout, info.import=info.import)
